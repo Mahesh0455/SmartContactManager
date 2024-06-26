9450b7b4d850b64baf5c7854fb684245494eefcd
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2024-06-27 00:05:54 +0530

    Adding project to repo

---
+++ src/main/java/com/smart/entities/User.java
@@ -0,0 +1,138 @@
+package com.smart.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aspectj.weaver.tools.Trace;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_reference")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private String password;
+	
+	private String role;
+	
+	private String imageUrlString;
+	
+	@Column(length = 500)
+	private String  about;
+	
+	private boolean enabled;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int id, String name, String email, String password, String role, String imageUrlString, String about,
+			boolean enabled) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.imageUrlString = imageUrlString;
+		this.about = about;
+		this.enabled = enabled;
+	}
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY ,mappedBy = "user")
+	private List<Contact> contacts=new ArrayList<Contact>();
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getImageUrlString() {
+		return imageUrlString;
+	}
+
+	public void setImageUrlString(String imageUrlString) {
+		this.imageUrlString = imageUrlString;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	
+
+}
