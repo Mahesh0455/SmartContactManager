f58d506d199b2c8851a28578d61c03c3922aeffd
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2024-07-18 00:18:37 +0530

    spring Security

---
+++ src/main/java/com/smart/config/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.smart.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository; 
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		
+		User user=(User) userRepository.getUserByName(username);
+		
+		
+		if(user==null) {
+			
+			
+			throw new UsernameNotFoundException(" Cound Not found User: "+username);
+		}
+		
+		CustomUserDetails customUserDetails=new CustomUserDetails(user);
+		
+		
+	
+		return customUserDetails;
+	}
+	
+	
+
+}
